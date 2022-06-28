cbc615f37eb826a032e2378a81c8b0ee751090e3
nir <nrbnlulu@gmail.com>
2022-06-28 15:51:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to build and install Pyside6 from source on ubuntu:
+### 1. download qt online installer from qt site
+### 2. change the qtpaths from the script to your path, should be somrhing like the path in the script
+### 3. run the script:
+`source install_pyside6.sh`
+
---
+++ install_pyside6.sh
@@ -0,0 +1,10 @@
+# run with venv
+wget https://download.qt.io/development_releases/prebuilt/libclang/libclang-release_100-based-linux-Rhel7.6-gcc5.3-x86_64.7z
+7z x libclang-release_100-based-linux-Rhel7.6-gcc5.3-x86_64.7z
+export CLANG_INSTALL_DIR=$PWD/libclang
+git clone --recursive https://code.qt.io/pyside/pyside-setup
+cd pyside-setup && git checkout 6.3.1
+source ../venv/bin/activate
+python -m pip install -r requirements.txt
+python -m pip install setuptools==62.3.3
+python setup.py install --qtpaths=/home/nir/Qt/6.3.1/gcc_64/bin/qtpaths --build-tests --ignore-git --parallel=8
